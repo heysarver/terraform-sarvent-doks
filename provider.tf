c11992dbd9824d4bf93dd5ee2dbd9722e7240f54
astromodder <22250203+astromodder@users.noreply.github.com>
2023-10-26 18:31:58 -0400

    a

--- provider.tf
+++ provider.tf
@@ -15,19 +15,15 @@ terraform {
   }
 }
 
-locals {
-  cluster_endpoint = digitalocean_kubernetes_cluster.main.endpoint
-}
-
 provider "kubernetes" {
-  host                   = local.cluster_endpoint
+  host                   = digitalocean_kubernetes_cluster.main.endpoint
   token                  = digitalocean_kubernetes_cluster.main.kube_config.0.token
   cluster_ca_certificate = base64decode(digitalocean_kubernetes_cluster.main.kube_config.0.cluster_ca_certificate)
 }
 
 provider "helm" {
   kubernetes {
-    host                   = local.cluster_endpoint
+    host                   = digitalocean_kubernetes_cluster.main.endpoint
     client_certificate     = base64decode(digitalocean_kubernetes_cluster.main.kube_config.0.client_certificate)
     client_key             = base64decode(digitalocean_kubernetes_cluster.main.kube_config.0.client_key)
     cluster_ca_certificate = base64decode(digitalocean_kubernetes_cluster.main.kube_config.0.cluster_ca_certificate)
