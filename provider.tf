263013f32c4ab764879657469ffead3f9c9e1853
astromodder <22250203+astromodder@users.noreply.github.com>
2023-10-26 17:33:15 -0400

    THERES the auth

--- provider.tf
+++ provider.tf
@@ -22,3 +22,12 @@ provider "kubernetes" {
     digitalocean_kubernetes_cluster.main.kube_config[0].cluster_ca_certificate
   )
 }
+
+provider "helm" {
+  kubernetes {
+    host = "${digitalocean_kubernetes_cluster.testcluster.endpoint}"
+    client_certificate     = "${base64decode(digitalocean_kubernetes_cluster.main.kube_config[0].client_certificate)}"
+    client_key             = "${base64decode(digitalocean_kubernetes_cluster.main.kube_config[0].client_key)}"
+    cluster_ca_certificate = "${base64decode(digitalocean_kubernetes_cluster.main.kube_config[0].cluster_ca_certificate)}"
+  }
+}
