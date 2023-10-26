05360d456c766ba6c1ac3671c7b001faba0305cb
astromodder <22250203+astromodder@users.noreply.github.com>
2023-10-26 17:47:30 -0400

    undo that one for now

--- provider.tf
+++ provider.tf
@@ -17,20 +17,19 @@ terraform {
 
 locals {
   cluster_endpoint = digitalocean_kubernetes_cluster.main.endpoint
-  kube_config = digitalocean_kubernetes_cluster.main.kube_config.0
 }
 
 provider "kubernetes" {
   host                   = local.cluster_endpoint
-  token                  = local.kube_config.token
-  cluster_ca_certificate = base64decode(local.kube_config.cluster_ca_certificate)
+  token                  = digitalocean_kubernetes_cluster.main.kube_config.0.token
+  cluster_ca_certificate = base64decode(digitalocean_kubernetes_cluster.main.kube_config.0.cluster_ca_certificate)
 }
 
 provider "helm" {
   kubernetes {
     host                   = local.cluster_endpoint
-    client_certificate     = base64decode(local.kube_config.client_certificate)
-    client_key             = base64decode(local.kube_config.client_key)
-    cluster_ca_certificate = base64decode(local.kube_config.cluster_ca_certificate)
+    client_certificate     = base64decode(digitalocean_kubernetes_cluster.main.kube_config.0.client_certificate)
+    client_key             = base64decode(digitalocean_kubernetes_cluster.main.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(digitalocean_kubernetes_cluster.main.kube_config.0.cluster_ca_certificate)
   }
 }
